ff5690c7aa5e79e9df40cd50285219e838524f8c
Lukas Jerusalem <lukasjerusalem@web.de>
2023-10-01 09:20:38 +0100

    form validation for mobile

--- script.js
+++ script.js
@@ -5,12 +5,22 @@ function emailCheck(event) {
   if (!re.test(email.value)){
     email.style.borderColor = "#ff5263"
     document.getElementById("errorMessage").style.visibility = "visible";
+    if(window.screen.availWidth < 780) {
+      document.getElementById("errorMessage").style.position = "relative";
+      document.getElementById("errorMessage").style.bottom = "8.2rem";
+      document.getElementById("submit").style.position = "relative";
+      document.getElementById("submit").style.top = "1rem";
+    }
   }
 }
 
 function takeError(event) {
   email.style.borderColor = ""
   document.getElementById("errorMessage").style.visibility = "";
+  document.getElementById("errorMessage").style.position = "";
+  document.getElementById("errorMessage").style.bottom = "";
+  document.getElementById("submit").style.position = "";
+  document.getElementById("submit").style.top = "";
 }
 
 document.getElementById("submit").addEventListener("mouseover", emailCheck);
